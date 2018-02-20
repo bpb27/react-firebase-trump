c37ffb076e3e278eca443c67663cda74c8ae0606
Brendan Brown <brendanbrown27@gmail.com>
2018-02-19 22:24:56 -0500

    updating lotsa stuff

--- app/src/actions/fetch-tweets.js
+++ app/src/actions/fetch-tweets.js
@@ -2,15 +2,12 @@ import * as Constants from '../constants';
 
 export default function (userId) {
   return async (dispatch, getState) => {
-
     const { db } = getState();
-
     const response = await db.ref(`/tweets/${userId}/`)
       .orderByChild('year')
       .equalTo('2018')
       .once('value')
 
     dispatch({ type: Constants.TWEETS, data: Object.values(response.val() || {}) });
-
   };
 };
--- app/src/components/page-home/index.js
+++ app/src/components/page-home/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import embeddedTweets from '../../utils/twitter-embed';
 
 class PageHome extends Component {
+
+  componentDidMount () {
+    embeddedTweets();
+  }
+
   render () {
     return (
-      <h1>Home page</h1>
+      <div className="page page-home">
+        <h1>Home page</h1>
+        <div className="timeline">
+          <a className="twitter-timeline" href="https://twitter.com/realDonaldTrump?ref_src=twsrc%5Etfw">Tweets by realDonaldTrump</a>
+        </div>
+      </div>
     );
   }
 }
