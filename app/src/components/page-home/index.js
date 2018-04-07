f6eee8c8d0f3710afb663bb1668e029018327ff4
Brendan Brown <brendanbrown27@gmail.com>
2018-04-07 13:37:40 -0400

    minor styling

--- app/src/components/page-home/index.js
+++ app/src/components/page-home/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import FontAwesome from 'react-fontawesome';
 import Lists from '../lists';
 import embeddedTweets from '../../utils/twitter-embed';
 import siteVideo from '../../assets/site.mp4';
@@ -9,6 +10,35 @@ class PageHome extends Component {
 
   componentDidMount () {
     embeddedTweets();
+  }
+
+  state = {
+    video: true // meaningless flag to toggle re-render
+  }
+
+  get video () {
+    if (window.localStorage.getItem('video') === 'false') return;
+
+    return (
+      <div className="video">
+        <span onClick={() => this.hideVideo()}>
+          <FontAwesome name='window-close' size='2x'/>
+        </span>
+        <p><Link to="/archive">Search through all 30,000+ tweets</Link></p>
+        <video autoPlay loop>
+          <source src={siteVideo} type="video/mp4" />
+        </video>
+      </div>
+    );
+  }
+
+  hideVideo () {
+    window.localStorage.setItem('video', false);
+    this.setState({ video: true });
+  }
+
+  shouldShowVideo () {
+    return window.localStorage.getItem('video') !== false;
   }
 
   render () {
@@ -24,12 +54,7 @@ class PageHome extends Component {
             </div>
           </div>
           <div className="right">
-            <div className="video">
-              <p><Link to="/archive">Search through all 30,000+ tweets</Link></p>
-              <video autoPlay loop>
-                <source src={siteVideo} type="video/mp4" />
-              </video>
-            </div>
+            { this.video }
             <Lists.PersonalSuperlatives/>
             <Lists.KeyIngredient/>
             <Lists.GlobalWarming/>
